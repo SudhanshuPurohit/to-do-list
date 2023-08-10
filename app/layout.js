6b4e6bf00df565b44f55ed2418e09a86e456595f
Sudhanshu Purohit <sudhanshup.nmh@gmail.com>
2023-08-10 18:21:28 +0530

    todoApp_1(update_myself)

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body suppressHydrationWarning>
+        {children}
+      <ToastContainer />
+      </body>
+      
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,95 +1,149 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+const page = () => {
+  // This are called states.
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [status, setStatus] = useState("due");
+  const [task, setTask] = useState([]);
+  const [updateAdd, setUpdateAdd] = useState("Add");
+  
+  // When task is created this functin is called to make a new task and add it in the task array(state).
+  var submitHandler = (e)=>{
+    e.preventDefault();
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    if(title.length < 5 || desc.length <15){
+      console.log(title.length , desc.length);
+      toast.error("The Title and Description must be the length of 5 and 15 atleast respectively");
+      return;
+    }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    var newTask = {
+      title,
+      desc,
+      status,
+      date: new Date().toLocaleDateString(),
+    }
+    
+    // const newTaskarr = [...task];
+    // newTaskarr.push(newTask);
+    // setTask(newTask);
+    // instead of above three line use
+    // copy using the derefernce operator and add newTask at last of it, and put it in task state using setTask.
+    setTask([...task, newTask]);
+    
+    
+    // Make  form  empty
+    setDesc("");
+    setStatus("due");
+    setTitle("");
+    setUpdateAdd("Add");
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+    // console.log(newTask)
+  }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+  // To show case the task in form of cards.
+
+  let taskList = <h4 className='text-center'>No Task To Show</h4>;
+
+    if(task.length>0){
+    taskList = task.map((item, index)=> {
+      return   (   <div key = {index} className="card  mb-3 w-100" >
+      <div className="card-body">
+          <h5 className="card-title">{item.title}</h5>
+
+          <p className="card-text">
+             {item.desc} hello
+
           </p>
-        </a>
+          <button onClick={()=> updateTask(index)} className="me-2 btn btn-sm   btn-dark">
+              Update Task
+          </button>
+          <button onClick={()=>deleteTask(index)} className="btn btn-sm btn-dark">
+              Delete Task
+          </button>
       </div>
-    </main>
+  </div>);
+    })
+    }
+
+
+    // Delete the Task:-
+
+    let deleteTask = (index)=> {
+      // const copyTask = [...task];
+      // copyTask.splice(index, 1);
+      // setTask(copyTask);
+
+      // Instead of above line write:
+      setTask(task.filter((e,i)=> i!==index));
+
+
+    }
+
+    let updateTask = (index)=>{
+      const {title, description, status} = tasks[index];
+
+      setTitle(title);
+      setDesc(description);
+      setStatus(status);
+
+      deleteTask(index);
+      setUpdateAdd("Update");
+
+    }
+
+  
+
+  // var titleHandler = (e)=>{
+  //   console.log(e.target.value);
+  //   setTitle(e.target.value);
+  // }
+
+//   var statusfun = (e)=>{
+//     setStatus(e.target.value);
+//      
+//   }
+
+// react toastify
+
+
+
+  
+  return (
+    <>
+    <h3 className='text-center mt-3'>TO-DO App</h3>
+    <div className="container p-5">
+      <h4>{updateAdd} Task</h4>
+      <form onSubmit={submitHandler} className=" d-flex-column align-items-center">
+        <input type="text" className="form-control mb-3" placeholder='Title' name='title' value={title} onChange={(e)=> setTitle(e.target.value)} />
+      
+        <input name="description" className="form-control mb-3" value={desc} placeholder='Description' onChange={(e)=>setDesc(e.target.value)}></input>
+      
+        <select name="status" className="form-select mb-3 dark" aria-label="Default select example" defaultValue={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="due">Due</option>
+          <option value="pending">Pending</option>
+          <option value="completed">Completed</option>
+        </select>
+      
+        <button className="btn btn-dark">{updateAdd} </button>
+
+      </form>
+
+      <br/>
+      <br/>
+      
+      <h4>Pending Tasks</h4>
+            <div className="d-flex flex-column flex-wrap">
+            {taskList} 
+            </div>
+            {/* {JSON.stringify(task)} */}
+    </div>
+    </>
   )
 }
+
+export default page;
